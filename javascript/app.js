9bd8c59692ba72a0ada37584ad5f21f1a7c1258c
unknown <godly.mg2@gmail.com>
2018-11-08 19:50:01 -0500

    Final Changes

--- javascript/app.js
+++ javascript/app.js
@@ -1,17 +1,29 @@
+$(document).ready(function () {
+
+
+	$("#pullDown").click(function () {
+		$("#panel").fadeToggle(1000);
+		$("#movie-list").fadeToggle(1000);
+
+
+	});
+
+	$("#datepicker").val( moment().format('YYYY-MM-DD') );
+        console.log(moment().format('MMM D, YYYY'));
+	// set a variable
+});
+
+
+
 $(document).foundation();
 var imageBaseUrl = 'https://image.tmdb.org/t/p/';
 $(document).ready(function () {
-	//var apiBaseURL = 'https://api.themoviedb.org/3/movie/335983/videos?api_key='
+
 	var apiBaseURL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=';
 	var apiTmdbkey = 'cf9588340d8a721412af021d7fc6ba6a';
 	var imageBaseUrl = 'https://image.tmdb.org/t/p/';
 
-	//swap 600 for ImageURL
-	//var imageURL = 'https://api.themoviedb.org/3/movie/600/images?api_key=cf9588340d8a721412af021d7fc6ba6a';
-	var youTubeLink = 'https://api.themoviedb.org/3/movie/335983/videos?api_key=cf9588340d8a721412af021d7fc6ba6a';
 
-	var tmdbURL = apiBaseURL + apiTmdbkey;
-	//console.log(tmdbURL);
 
 	var settings = {
 		"url": apiBaseURL + apiTmdbkey,
@@ -19,10 +31,7 @@ $(document).ready(function () {
 	}
 
 	$.ajax(settings).done(function (response) {
-		//	console.log(response);
-		//	console.log(response.results[0].overview);
-		//var overview = response.results[0].overview;
-
+			
 		for (i = 0; i < response.results.length; i++) {
 
 			document.getElementById('movie-grid').innerHTML += '<a data-toggle="exampleModal9"><img src="' + imageBaseUrl + 'w300' + response.results[i].poster_path + '" id = "' + response.results[i].id + '" onclick = "reveal(this.id)">';
@@ -47,9 +56,8 @@ function reveal(clickedId) {
 
 	$.ajax(settings).done(function (response) {
 		console.log(response);
-		//alert(response.overview);
 		var overview = response.overview;
-		document.getElementById('exampleModal9').innerHTML = '<div class="row"> <div class="cell small-4 large-4 columns"><img src="' + imageBaseUrl + 'w300' + response.poster_path + '"><div class="cell small-4 large-4 columns">' + overview + '</div></div><button id =' + clickedId + ' class="close-button" data-close aria-label="Close reveal" type="button"><span aria-hidden="true">&times</span></button><a href="#" data-reveal-id="videoModal" id =' + clickedId + ' class="radius button" onclick = "youTubeReveal(this.id)">Watch Trailer&hellip;</a><input type = "text"><script>$(document).foundation();</script>';
+		document.getElementById('exampleModal9').innerHTML = '<div class="row"> <div class="cell small-4 large-4 columns"><img src="' + imageBaseUrl + 'w300' + response.poster_path + '"><div class="cell small-4 large-4 columns overview">' + overview + '</div></div><button id =' + clickedId + ' class="close-button" data-close aria-label="Close reveal" type="button"><span aria-hidden="true">&times</span></button><a href="#" data-reveal-id="videoModal" id =' + clickedId + ' class="radius button" onclick = "youTubeReveal(this.id)">Watch Trailer&hellip;</a><script>$(document).foundation();</script>';
 
 	});
 }
@@ -62,8 +70,6 @@ function youTubeReveal(clickedId) {
 	}
 	var exampleModal8 = document.getElementById(clickedId).value;
 	$.ajax(settings).done(function (response) {
-		//console.log(response);
-		//alert(response.resultskey);
 		document.getElementById('exampleModal9').innerHTML = '<iframe width="553" height="280" src="https://www.youtube.com/embed/' + response.results[0].key + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><button id =' + clickedId + ' class="close-button" data-close aria-label="Close reveal" type="button"><span aria-hidden="true">&times</span></button>';
 
 
@@ -93,6 +99,7 @@ $("#movieSearchBtn").on('click', function (event) {
 
 	});
 });
+
 //Genre
 $(".genre").on('click', function (event) {
 	event.preventDefault();
@@ -113,38 +120,69 @@ $(".genre").on('click', function (event) {
 	});
 })
 
-//http://api.fandango.com/v1/?op=theatersbypostalcodesearch&postalcode=94105&apikey=&sig=7e56dd10e7d394376ac564563086c205a21071f5adda61c8e419a9cd3c2b53b3
-//Fandango
-// $("#zipCode").on('click', function (event) {
-// 	event.preventDefault();
 
-// 	console.log('Getting here');
-// 	var settings = {
+//Search by Zip Code
+$("#zipCode").on('click', function (event) {
 
-// 		"url": "http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-11-07&zip=78501&api_key=gvfz63zjy3q4pk92hdhxeekg",
-// 		"method": "GET"
-// 	}
+	event.preventDefault();
+	$("#select").empty();
 
-// 	$.ajax(settings).then(function (response) {
-// 		console.log('Fandango:' +response);
-// 	});
+	var zipCode = $("#zipSearch").val().trim();
+	var today = $("#datepicker").val().trim();
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://data.tmsapi.com/v1.1/movies/showings?startDate="+today+"&zip="+zipCode+"&api_key=gvfz63zjy3q4pk92hdhxeekg",
+		"method": "GET"
+	}
 
-// var invocation = new XMLHttpRequest();
-// var url = 'https://crossorigin.me/http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-11-07&zip=78501&api_key=gvfz63zjy3q4pk92hdhxeekg';
-    
-// function callOtherDomain(){
-//   if(invocation) {
-//     invocation.open('GET', url, true);
-//     invocation.withCredentials = true;
-//     invocation.onreadystatechange = function () {
-// 		console.log(invocation.responseText);
-// 	} ;
-//     invocation.send(); 
-//   }
-// }
+	$.ajax(settings).done(function (response) {
 
-// })
+		
+		var options = '';
+		var options2 = '';
+		var options3;
+		var options4;
+		var tr='<tr>';
+		var tr2='</tr>';
+		var td='<td>';
+		var td2 = '</td>';
+		var datetime;
+		for (i = 0; i < response.length; i++) {
+				options='<td>'+response[i].title+'</td>'; 
 
+				for (j=0;j<response[i].showtimes.length;j++)
+				{
+					datetime=response[i].showtimes[j].dateTime.substring(11)
+
+					
+					options2+='<p>'+response[i].showtimes[j].theatre.name+'</p>';
+					options4+='<p>'+datetime+'</p>';
+				}
+				
+				options3=tr+options+td+options2+td2+td+options4+td2+tr2;
+				$("#movie-list").prepend(options3);
+
+				options2='';
+				options4='';
+						
+				
+		}
+		
+	});
+
+
+})
+
+//Form Validation
+function myFunction() {
+    var inpObj = document.getElementById("zipSearch");
+    if (!inpObj.checkValidity()) {
+        document.getElementById("demo").innerHTML = 'Please Enter a valid zip Code';
+    } else {
+        document.getElementById("demo").innerHTML = "Input OK";
+    } 
+} 
 
 
 
@@ -154,10 +192,6 @@ $(document).foundation();
 
 window.onload = function () {
 	stickyFooter();
-
-	//you can either uncomment and allow the setInterval to auto correct the footer
-	//or call stickyFooter() if you have major DOM changes
-	//setInterval(checkForDOMChange, 1000);
 };
 
 //check for changes to the DOM
@@ -181,7 +215,7 @@ function getCSS(element, property) {
 
 	} else if (window.getComputedStyle) {
 		css = document.defaultView.getComputedStyle(elem, null).
-			getPropertyValue(property);
+		getPropertyValue(property);
 	}
 
 	return css;
@@ -212,5 +246,5 @@ function stickyFooter() {
 }
 
 /*
-! end sticky footer
+! end sticky footer 	
 */
